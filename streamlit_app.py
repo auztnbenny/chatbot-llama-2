3926feee082350edeae30b577bcb5291f2a6a7b4
Austin Benny <austinbenny2025@cs.sjcetpalai.ac.in>
2024-08-02 07:14:26 +0530

    second

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,25 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot')
     st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
-    replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    if replicate_api:
-        if replicate_api.startswith('r8_') and len(replicate_api) == 40:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+    
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            st.warning('Please enter a valid API key!', icon='⚠️')
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-    # Parameters
-    temperature = st.sidebar.slider('Temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('Top P', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('Max Length', min_value=32, max_value=128, value=120, step=8)
+    st.subheader('Models')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+    elif selected_model == 'Llama2-13B':
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
 
 # Load FAQ data from JSON file
 with open('data.json') as f:
@@ -48,17 +55,9 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    
-    output = replicate.run(
-        'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-        input={
-            "prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-            "temperature": temperature,
-            "top_p": top_p,
-            "max_length": max_length,
-            "repetition_penalty": 1
-        }
-    )
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature": 0.1, "top_p": 0.9, "max_length": 120, "repetition_penalty": 1})
     return output
 
 # User-provided prompt
@@ -67,16 +66,21 @@ if prompt := st.chat_input(disabled=not replicate_api):
     with st.chat_message("user"):
         st.write(prompt)
 
-    # Generate a new response if last message is not from assistant
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = generate_llama2_response(prompt)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
+    # Check if the prompt matches any FAQ
+    if prompt in faq_data:
+        response = faq_data[prompt]
+    else:
+        # Generate a new response if the last message is not from assistant
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = generate_llama2_response(prompt)
+                    placeholder = st.empty()
+                    full_response = ''
+                    for item in response:
+                        full_response += item
+                        placeholder.markdown(full_response)
                     placeholder.markdown(full_response)
-                placeholder.markdown(full_response)
+        
         message = {"role": "assistant", "content": full_response}
         st.session_state.messages.append(message)
